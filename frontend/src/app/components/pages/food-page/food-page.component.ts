9cd5ecfc861861aa94f8e731b0246db26e148f9f
spider0204 <saket.tiwari02042001@gmail.com>
2023-08-04 23:55:23 +0530

    added cart-page

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/food';
 
@@ -8,15 +9,28 @@ import { Food } from 'src/app/shared/models/food';
   templateUrl: './food-page.component.html',
   styleUrls: ['./food-page.component.css']
 })
-export class FoodPageComponent {
+export class FoodPageComponent implements OnInit{
 
   food!:Food;
-  constructor(activatedRoute:ActivatedRoute,foodService:FoodService){
+  constructor(activatedRoute:ActivatedRoute,foodService:FoodService,private cartService:CartService,private router:Router){
     activatedRoute.params.subscribe((params)=>{
            if(params['id'])
            this.food= foodService.getFoodById(params['id']);
     })
 
   }
+
+  ngOnInit(): void {
+    
+  }
+
+  addToCart(){
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  }
+
+
+
+  
 
 }
